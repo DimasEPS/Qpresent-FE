c7bbfa3b541859359b40fb23dce5c4fe37ed0c87
DimasEPS <dimaskld4@gmail.com>
2025-11-15 16:41:19 +0700

    routing and tailwind setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-2xl font-black">Welcome to Qpresent</div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <div className="font-bold text-blue-400">Login Page</div>
+    </div>
+  );
+}
+
+export default Login;
